774e1742c88b9056d3903fc16915c6f83b8f12e5
pavelromci25 <pavel.romci25@gmail.com>
2025-04-07 22:51:27 +0300

    Simplify App.tsx for debugging

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import DeveloperDashboard from './components/DeveloperDashboard';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <Routes>
-          <Route path="/" element={<DeveloperDashboard />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="app-container">
+      <h1>Hello, Nebula Developer!</h1>
+    </div>
   );
 }
 
